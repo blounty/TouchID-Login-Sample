249c4bf813e5e5b20ab75fc882293fcf76386d83
Alex Blount <alexkblount@gmail.com>
2014-07-11 14:20:10 +1000

    inital commit

---
+++ TouchIDLogin/TouchIDLoginViewController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using MonoTouch.LocalAuthentication;
+
+namespace TouchIDLogin
+{
+	public partial class TouchIDLoginViewController : UIViewController
+	{
+		public TouchIDLoginViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		#region View lifecycle
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+		}
+
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+		}
+
+		public override void ViewDidDisappear (bool animated)
+		{
+			base.ViewDidDisappear (animated);
+		}
+
+
+		partial void UIButton7_TouchUpInside (UIButton sender)
+		{
+			var context = new LAContext ();
+
+			var error = new NSError ();
+			if (context.CanEvaluatePolicy (LAPolicy.DeviceOwnerAuthenticationWithBiometrics, error)) {
+				var replyHandler = new LAContextReplyHandler((success, err) => {
+					this.InvokeOnMainThread(() => {
+						if(success){
+							Console.WriteLine("You Logged in");
+						} else {
+							var errorAlertView = new UIAlertView("Login Error", err.LocalizedDescription, null, "Close");
+							errorAlertView.Show();
+						}
+					});
+				});
+				context.EvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, "You need to login", replyHandler);
+			}
+		}
+		#endregion
+	}
+}
+
---
+++ TouchIDLogin/TouchIDLoginViewController.designer.cs
@@ -0,0 +1,25 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
+
+namespace TouchIDLogin
+{
+	[Register ("TouchIDLoginViewController")]
+	partial class TouchIDLoginViewController
+	{
+		[Action ("UIButton7_TouchUpInside:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void UIButton7_TouchUpInside (UIButton sender);
+
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
